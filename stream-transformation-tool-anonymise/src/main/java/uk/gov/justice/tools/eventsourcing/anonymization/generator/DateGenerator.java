8430de24a42e4ce7c43c6f44822f266cd9834f01
Yashdeep Srivastava <yashdeep@scrumconnect.com>
2020-02-21 11:19:30 +0000

    CPI-81 | Yashdeep/Ashwin | Adds date converter to set day and month as 01

--- stream-transformation-tool-anonymise/src/main/java/uk/gov/justice/tools/eventsourcing/anonymization/generator/DateGenerator.java
+++ stream-transformation-tool-anonymise/src/main/java/uk/gov/justice/tools/eventsourcing/anonymization/generator/DateGenerator.java
@@ -1,10 +1,15 @@
 package uk.gov.justice.tools.eventsourcing.anonymization.generator;
 
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DateGenerator extends Generator<String> {
 
     @Override
     public String convert(final String fieldValue) {
-        return fieldValue;
+        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        final LocalDate date = LocalDate.parse(fieldValue, dateTimeFormatter);
+        return date.withMonth(1).withDayOfMonth(1).toString();
     }
 }
